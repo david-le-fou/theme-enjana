6f0db6809b86b6d5f49550fd4941ac289cdf8596
david.propitech <david.propitech@gmail.com>
2022-03-17 16:43:03 +0300

    master

--- functions.php
+++ functions.php
@@ -2,14 +2,42 @@
 function register_my_menu(){
     register_nav_menu( 'main-menu', 'Menu principal' );
   }
-
 add_action( 'after_setup_theme', 'register_my_menu' );
 function charger_bootstrap() {
-
-    wp_enqueue_script( 'bootstrap-js', get_stylesheet_directory_uri() . '/lib/bootstrap.js', array( 'jquery' ), null, true );
-    
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/lib/bootstrap.js');
+    wp_enqueue_script( 'javascript', get_template_directory_uri().'/javascript.js' );
     wp_enqueue_style( 'bootstrap-css', get_stylesheet_directory_uri() . '/lib/bootstrap3.css' );
-    
-}
-    
+}   
 add_action('wp_enqueue_scripts', 'charger_bootstrap');
+
+function modif_head(){
+  // add_menu_page( string $page_title, 
+  //                string $menu_title, 
+  //                string $capability, 
+  //                string $menu_slug, 
+  //                callable $function = '', 
+  //                string $icon_url = '', 
+  //                int $position = null )
+  add_menu_page('modification header',
+  'Header', 
+  'manage_options', 
+  'modif-head',
+  'head_modif', 
+  'dashicons-book-alt');
+
+  add_menu_page('modification footer',
+  'Footer', 
+  'manage_options', 
+  'modif-footer',
+  'head_modif', 
+  'dashicons-book-alt');
+}
+add_action('admin_menu', 'modif_head');
+function head_modif(){
+  echo "test header";
+}
+
+add_action('admin_menu', 'modif_footer');
+function footer_modif(){
+  echo "test";
+}
--- header.php
+++ header.php
@@ -7,13 +7,52 @@
 </head>
 
 <body>
-    <nav id="navigation">
-    <?php
-        wp_nav_menu(
-            array(
-                'theme_location' => 'main-menu',
-                'menu_id' => 'primary-menu',
-            )
-        );
-    ?>
-    </nav>
+<div class="content">
+
+    <div class="row">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+        <nav id="navigation" class="navbar navbar-default">
+        <!-- wp_nav_menu( array $args = array(
+                        'menu'              => "", // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
+                        'menu_class'        => "", // (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
+                        'menu_id'           => "", // (string) The ID that is applied to the ul element which forms the menu. Default is the menu slug, incremented.
+                        'container'         => "", // (string) Whether to wrap the ul, and what to wrap it with. Default 'div'.
+                        'container_class'   => "", // (string) Class that is applied to the container. Default 'menu-{menu slug}-container'.
+                        'container_id'      => "", // (string) The ID that is applied to the container.
+                        'fallback_cb'       => "", // (callable|bool) If the menu doesn't exists, a callback function will fire. Default is 'wp_page_menu'. Set to false for no fallback.
+                        'before'            => "", // (string) Text before the link markup.
+                        'after'             => "", // (string) Text after the link markup.
+                        'link_before'       => "", // (string) Text before the link text.
+                        'link_after'        => "", // (string) Text after the link text.
+                        'echo'              => "", // (bool) Whether to echo the menu or return it. Default true.
+                        'depth'             => "", // (int) How many levels of the hierarchy are to be included. 0 means all. Default 0.
+                        'walker'            => "", // (object) Instance of a custom walker class.
+                        'theme_location'    => "", // (string) Theme location to be used. Must be registered with register_nav_menu() in order to be selectable by the user.
+                        'items_wrap'        => "", // (string) How the list items should be wrapped. Default is a ul with an id and class. Uses printf() format with numbered placeholders.
+                        'item_spacing'      => "", // (string) Whether to preserve whitespace within the menu's HTML. Accepts 'preserve' or 'discard'. Default 'preserve'.
+                    ) ); -->
+            <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'main-menu',
+                        'menu_class' => 'nav navbar-nav',
+                        'item_spacing'  => 'test',
+                        'menu_id' => 'primary-menu',
+                        'sub_menu'
+                    )
+                );
+            ?>
+             <?php wp_nav_menu(array(
+                'theme_location'=>'main-menu',
+                'depth' => 3,
+                'container' => 'ul',
+                'container_class' => 'main-menu ',
+                'menu_class'=>'navigation-menu', 
+                'link_before' => '<i class="ti-home"></i><span>', 
+                'link_after' => '</span>',
+
+                )); ?>
+        </nav>
+        </div>
+    </div>
+    
